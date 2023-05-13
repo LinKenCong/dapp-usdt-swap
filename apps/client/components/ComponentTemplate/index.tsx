9052902adc8d9699ad4678315015e5cfcd35c578
kyrie <lamkencong@gmail.com>
2023-05-13 22:58:40 +0800

    update filename ComponentTemplate

---
+++ apps/client/components/ComponentTemplate/index.tsx
@@ -0,0 +1,10 @@
+import sty from "./style.module.scss";
+
+const ComponentTemplate = () => {
+  return (
+    <>
+      <div className={sty.ComponentTemplate}>Section</div>
+    </>
+  );
+};
+export default ComponentTemplate;
