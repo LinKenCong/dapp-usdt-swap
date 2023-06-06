ad48a31ded42c218c34d83e9d17cb1b272db67b8
kyrie <lamkencong@gmail.com>
2023-06-06 23:09:21 +0800

    update client -> add logo

--- apps/client/components/InfoCard/index.tsx
+++ apps/client/components/InfoCard/index.tsx
@@ -1,12 +1,16 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import sty from "./style.module.scss";
+import Logo from "../../assets/logo.png";
+import Image from "next/image";
 
 const InfoCard = () => {
   return (
     <>
       <div className={sty.InfoCard}>
         <div className={sty.websiteInfo}>
-          <div className={sty.logoPart}></div>
+          <div className={sty.logoPart}>
+            <Image src={Logo} width={80} height={80} />
+          </div>
           <div className={sty.titlePart}></div>
         </div>
         <div className={sty.userInfo}>
